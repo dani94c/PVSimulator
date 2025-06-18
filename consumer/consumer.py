24729288a988231ccb513c63478bad755cb8d7f5
dani94c <danielacomola@gmail.com>
2025-06-18 23:39:55 +0200

    Basic implementation of the meter simulator + fix on containers starts conditions

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,4 +1,15 @@
 import pika, time
+import signal
+
+def shutdown_handler(signum, frame):
+    print("\nSginal received. Closing...")
+    try:
+        channel.stop_consuming()
+    except Exception:
+        pass
+
+signal.signal(signal.SIGINT, shutdown_handler)
+signal.signal(signal.SIGTERM, shutdown_handler)
 
 
 try:
@@ -7,17 +18,21 @@ try:
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq-host', port=5672))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='home_power_data')
 
     print("Connected! Listening for messages...")
 
     def callback(ch, method, properties, body):
         print(f"Received {body}")
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='home_power_data', on_message_callback=callback, auto_ack=True)
 
     print('Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
-except KeyboardInterrupt:
-    print("Closing...")
+finally:
+    if channel.is_open:
+        channel.close()
+    if connection.is_open:
+        connection.close()
+    print("PV SIMULATOR || All connections are closed")
     exit(0)
--- producer/producer.py
+++ producer/producer.py
@@ -1,19 +1,86 @@
 import pika
 from datetime import datetime
 import time
+import random
+import json
+import signal
+import sys
+
+
+#Handle clean clousure of active connection to rabbitmq broker
+def shutdown_handler(sig, frame):
+    print("\nSignal received, closing connections...")
+    if channel.is_open:
+        channel.close()
+    if connection.is_open:
+        connection.close()
+    print("Connection closed.")
+    sys.exit(0)
+
+# Register handler for received signals 
+signal.signal(signal.SIGINT, shutdown_handler)
+signal.signal(signal.SIGTERM, shutdown_handler)
+
+#Handling uncaught exception
+def handle_uncaught_exception(exc_type, exc_value, exc_traceback):
+    # Logs lo stack trace
+    print(f"Uncaught exception:, {exc_type}, {exc_value}, {exc_traceback}")
+
+sys.excepthook = handle_uncaught_exception
+
+#Mimics actual household consumption patterns generating random, but continues, values (values change smoothly over time)
+class MeterSimulator:
+    def __init__(self):
+        self.current_power = 2.0      #Start at 2kW (reasonable baseline for house power conumptions)
+        
+    def get_next_reading(self):
+        # Small random change (±10% of current value, max ±0.5kW)
+        max_change = min(0.5, self.current_power * 0.1)
+        change = random.uniform(-max_change, max_change)
+        
+        self.current_power = max(0.0, min(10.0, self.current_power + change))
+        return round(self.current_power, 2)
+
+
 
 #time.sleep(10)
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='rabbitmq-host', port=5672))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='home_power_data')
 
 #msg = f"{datetime.now().strftime("%d/%m/%Y, %H:%M:%S")} Hello World!"
 
-for i in range(0,5):
-    channel.basic_publish(exchange='', routing_key='hello', body=f"[{datetime.now().strftime("%d/%m/%YT%H:%M:%S")}] Hello World!")
-    print(f"[{i}] Sent Hello World")
-    time.sleep(3)
+# for i in range(0,5):
+#     channel.basic_publish(exchange='', routing_key='hello', body=f"[{datetime.now().strftime("%d/%m/%YT%H:%M:%S")}] Hello World!")
+#     print(f"[{i}] Sent Hello World")
+#     time.sleep(3)
 
-connection.close()
+meter = SimpleMeterSimulator()
+
+while True:
+    power_reading = meter.get_next_reading()
+    sampling_timestamp = datetime.now().isoformat()
+
+    print(f'{sampling_timestamp} || METER SIMULATOR || New power consumption generated: {power_reading}')
+    
+    message = {
+        #meter_id
+        "timestamp": sampling_timestamp,
+        "meter_power_kw": power_reading,
+        "type": "house_meter_reading"
+    }
+    
+    try:
+        channel.basic_publish(
+            exchange='',
+            routing_key='home_power_data',
+            body=json.dumps(message)
+        )
+        print(f'Published meter power value: {power_reading} kW')
+    except Exception as e:
+        print(f'Failed to publish message: {e}')
+
+    
+    time.sleep(2)  # Reading every 2 seconds
